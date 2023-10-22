be676f030afdbf2986e3eef1ee888d66c09f2dd2
PeakyBalwinder <133050753+PeakyBalwinder@users.noreply.github.com>
2023-10-22 16:39:59 +0530

    Re-submission blinkycode.ino

--- blinkycode.ino
+++ blinkycode.ino
@@ -1,20 +1,20 @@
-const int buttonPin = 2;  // Change this to the pin you connect the button to
-bool buttonPressed = false;
+const int buttonPin = 2;  
+bool buttonPressed = false;  // Flag to track if the button is pressed
 
 void dot()
 {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(200);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH);  // Turns on the built-in LED
+  delay(200);  // Delay for 200 milliseconds
+  digitalWrite(LED_BUILTIN, LOW);  // Turns off the built-in LED
+  delay(100);  // Delay for 100 milliseconds
 }
 
 void dash()
 {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(600);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH);  // Turns on the built-in LED
+  delay(600);  // Delay for 600 milliseconds 
+  digitalWrite(LED_BUILTIN, LOW);  // Turns off the built-in LED
+  delay(100);  // Delay for 100 milliseconds
 }
 
 void blinkCharacter(char c)
@@ -48,28 +48,28 @@ void blinkCharacter(char c)
       dash();
       break;
     default:
-      // Handle other characters if needed
+      // Handles other characters if typed
       break;
   }
 }
 
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(buttonPin, INPUT_PULLUP);  // Using internal pull-up resistor
-  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);  // Sets the built-in LED pin as the output
+  pinMode(buttonPin, INPUT_PULLUP);  // Sets the button pin as an input 
+  Serial.begin(9600);  
 }
 
 void loop()
 {
-  buttonPressed = digitalRead(buttonPin) == LOW;
+  buttonPressed = digitalRead(buttonPin) == LOW;  // Checks if the button is pressed 
 
   if (buttonPressed)
   {
     Serial.println("Button pressed. Resetting...");
-    delay(1000);  // Debounce delay
+    delay(1000);  // Delay to give gap in between button pressing
     
-    // Clear any pending serial input
+    // Clears any pending serial input
     while (Serial.available())
     {
       Serial.read();
@@ -82,8 +82,8 @@ void loop()
   
   if (Serial.available() > 0)
   {
-    String name = Serial.readString();
-    name.toUpperCase();
+    String name = Serial.readString();  // Reads the incoming serial data as a string
+    name.toUpperCase();  // Convert the name to uppercase
 
     for (int i = 0; i < name.length(); i++)
     {
@@ -93,7 +93,7 @@ void loop()
       }
       else
       {
-        blinkCharacter(name[i]);
+        blinkCharacter(name[i]);  // Blinks the LED to represent the character
         delay(400); // Gap between characters
       }
     }
